3d57ce2a980781e93473cf2b98f4570bdba9390c
Krista-Mari Katajisto <kristamari.katajisto@gmail.com>
2022-02-02 10:52:37 +0200

    messageService imported

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
-// import { HEROES } from '../mock-heroes';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-
 export class HeroesComponent implements OnInit {
 
   selectedHero?: Hero;
